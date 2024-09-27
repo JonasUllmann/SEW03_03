4e956b76ba3a0ec8a27a4d94e1b8f505055bcab5
JonasUllmann <jonas.ullmann@josephinum.at>
2024-09-27 11:21:38 +0200

    Add project files.

---
+++ SEW03_03_01_FraktalesMuster/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace SEW03_03_01_FraktalesMuster
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ SEW03_03_02_Sierpinski-Dreieck/MainWindow.xaml.cs
@@ -0,0 +1,89 @@
+﻿using System.ComponentModel;
+using System.Reflection.Emit;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Woopec.Core;
+
+
+namespace SEW03_03_02_Sierpinski_Dreieck
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public static void TurtleMain()
+        {
+
+            int level = 8;
+            float startsize = 800;
+
+            Random rnd = new Random();
+            Woopec.Core.Color[] myColors = new Woopec.Core.Color[level];
+
+            for (int i = 0; i < level; i++)
+            {
+                myColors[i] = new Woopec.Core.Color(rnd.Next(0, 255), rnd.Next(0, 255), rnd.Next(0, 255));
+            }
+
+            drawPattern(startsize, 0, 0, level-1, myColors);
+            level--;
+            drawtri(startsize / 2, 0, 0, level, myColors);
+
+
+
+
+            static void drawline(float x0, float y0, float x1, float y1, Woopec.Core.Color myColor)
+            {
+                var turt = Turtle.Seymour();
+                turt.Speed = 100000;
+
+                turt.PenUp();
+                turt.GoTo((x0, y0));
+
+                turt.PenDown();
+                turt.Color = myColor;
+                turt.GoTo((x1, y1));
+                turt.HideTurtle();
+            }
+
+            static void drawPattern(float size, float x, float y, int level, Woopec.Core.Color[] myColors)
+            {
+                float half = size / 2;
+                drawline(x - half, y - half, x + half, y - half, myColors[level]);
+                drawline(x - half, y - half, x , y + half, myColors[level]);
+                drawline(x, y + half, x + half, y - half, myColors[level]);
+            }
+
+            static void drawtri(float size, float x, float y, int level, Woopec.Core.Color[] myColors)
+            {
+
+                if (level == -1)
+                {
+                    return;
+                }
+
+                float half = size / 2;
+
+                
+
+
+            }
+
+        }
+
+    }
+}
